fc330404309eb1419fcec9e4f5eef4270bd504fd
James Perry <j.perry@epcc.ed.ac.uk>
2019-09-04 09:34:35 +0100

    Allow test job to be submitted to single site, and switch to 8 core jobs

--- submit_site_test_jobs.py
+++ submit_site_test_jobs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # Submits the same job to each of the sites for testing
-# Arguments: visit number (8 digits); index
+# Arguments: visit number (8 digits); index; optional site (for single site)
 # The job IDs are recorded in a file named 'site_test_jobs.txt'.
 
 import sys
@@ -15,15 +15,21 @@ sites = [ "VAC.UKI-NORTHGRID-MAN-HEP.uk", "LCG.IN2P3-CC.fr", "LCG.RAL-LCG2.uk", 
 
 dirac = Dirac()
 
-if len(sys.argv) != 3:
-    print "Usage: submit_site_test_jobs.py <visit number> <index>"
+if len(sys.argv) < 3:
+    print "Usage: submit_site_test_jobs.py <visit number> <index> [<site>]"
     sys.exit(1)
 visit = sys.argv[1]
 idx = int(sys.argv[2])
 print "Submitting jobs for visit", visit, "index", idx
 
+listfile = 'site_test_jobs.txt'
+
+if len(sys.argv) == 4:
+    sites = [sys.argv[3]]
+    listfile = 'site_test_' + sys.argv[3] + '.txt'
+
 # open a file to record a list of job IDs
-joblistfile = open('site_test_jobs.txt', 'w')
+joblistfile = open(listfile, 'w')
 
 for site in sites:
     j = Job()
@@ -32,10 +38,10 @@ for site in sites:
     instcatname = visit + ".tar.gz"
     insidename = 'phosim_cat_' + str(int(visit)) + '.txt'
 
-    startsensor = idx * 4
-    numsensors = 4
-    if idx == 47:
-        numsensors = 1
+    startsensor = idx * 6
+    numsensors = 6
+    if idx == 41:
+        numsensors = 3
     
     args = visit + ' ' + insidename + ' ' + str(startsensor) + ' ' + str(numsensors) + ' ' + str(idx)
     outputname = 'fits_' + visit + '_' + str(idx) + '.tar'
@@ -45,9 +51,9 @@ for site in sites:
     j.stderr="std.err"
     j.stdout="std.out"
     #!!! May need the 2.1i directory here depending on visit number !!!
-    j.setInputSandbox(["runimsim2.1.sh","run_imsim_nersc.py","LFN:/lsst/user/j/james.perry/instcats/2.1i/" + instcatname])
+    j.setInputSandbox(["runimsim2.1.sh","run_imsim_nersc.py","LFN:/lsst/user/j/james.perry/instcats/2.1.1i/" + instcatname])
     j.setOutputSandbox(["std.out","std.err"])
-    j.setTag(["4Processors"])
+    j.setTag(["8Processors"])
     j.setOutputData([visit + "/" + outputname], outputPath="", outputSE=["UKI-NORTHGRID-LANCS-HEP-disk"])
     j.setPlatform("AnyPlatform")
 
